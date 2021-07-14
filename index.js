fb0e5bc83325c28dfdb0b181978f1cf8cc1a81c3
FidelMunywoki <fidelis.munywoki@student.moringaschool.com>
2021-07-14 13:44:45 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-
-const array = ["one", "two", "three"]
-array.forEach(function (item, index) {
-  console.log(item);
-});
+//getting information from the form.
+$(document).ready(function() {
+    $("form#form").submit(function(event) {
+        event.preventDefault();
+        var year = parseInt($("#year").val());
+        var month = parseInt($("#month").val());
+        var date = parseInt($("#date").val());
+        var gender = $("input:radio[name=gender]:checked").val();
+        var result = akanNames(year, month, date,gender)
+    })
+})
