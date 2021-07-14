8e0ff4447eaaa5b4beb3afbf942a910d4afc3f85
FidelMunywoki <fidelis.munywoki@student.moringaschool.com>
2021-07-14 23:25:06 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-//getting information from the form.
-$(document).ready(function() {
-    $("form#form").submit(function(event) {
-        event.preventDefault();
-        var year = parseInt($("#year").val());
-        var month = parseInt($("#month").val());
-        var date = parseInt($("#date").val());
-        var gender = $("input:radio[name=gender]:checked").val();
-        var result = akanNames(year, month, date,gender)
-    })
-})
+var month = document.getElementById("month");
+var day = document.getElementById("date");
+var year = document.getElementById("year");
+var gender = document.getElementsByName("gender");
+
+function akanNames(month, day, year, gender){
+    var dob = month + " " + day + ", " + year;
+    var then = new Date(dob);
+    var theDay = then.getDay();
+    //Days counting ===>>>>["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var akanMale = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+    var akanFemale = ['Akosua', 'Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+    if (gender === 'Male'){
+        console.log(akanMale[theDay]);
+    }
+    else if ( gender === 'Female'){
+        console.log(akanFemale[theDay]);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+var month = 'Jan';
+var day = 19;
+var year = 1995
+var dob = month + " " + day + ", " + year;
+var gender = 'Female'
+
+var then = new Date(dob);
+var theDay = then.getDay();
+//Days counting ===>>>>["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+console.log(theDay);
+
+var akanMale = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+var akanFemale = ['Akosua', 'Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+if (gender === 'Male'){
+    console.log(akanMale[theDay]);
+}
+else if ( gender === 'Female'){
+    console.log(akanFemale[theDay]);
+}
