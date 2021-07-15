7beee3466833169184be1736223dc98b86238f56
FidelMunywoki <fidelis.munywoki@student.moringaschool.com>
2021-07-15 20:25:05 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var month = document.getElementById("month");
-var day = document.getElementById("date");
-var year = document.getElementById("year");
-var gender = document.getElementsByName("gender");
 
-function akanNames(month, day, year, gender){
+function akanNames(){
+    var month = document.getElementById("month");
+    var day = document.getElementById("date");
+    var year = document.getElementById("year");
+    var gender = document.getElementsByName("gender");
     var dob = month + " " + day + ", " + year;
     var then = new Date(dob);
     var theDay = then.getDay();
@@ -11,40 +11,29 @@ function akanNames(month, day, year, gender){
     var akanMale = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
     var akanFemale = ['Akosua', 'Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
     if (gender === 'Male'){
-        console.log(akanMale[theDay]);
+        return akanMale[theDay];
     }
     else if ( gender === 'Female'){
-        console.log(akanFemale[theDay]);
+        return akanFemale[theDay];
     }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-var month = 'Jan';
-var day = 19;
-var year = 1995
-var dob = month + " " + day + ", " + year;
-var gender = 'Female'
-
-var then = new Date(dob);
-var theDay = then.getDay();
-//Days counting ===>>>>["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-console.log(theDay);
-
-var akanMale = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
-var akanFemale = ['Akosua', 'Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
-if (gender === 'Male'){
-    console.log(akanMale[theDay]);
+function outputAkanNames(){
+    var 
 }
-else if ( gender === 'Female'){
-    console.log(akanFemale[theDay]);
-}
+
+
+
+akanName = akanNames();
+console.log(akanName);
+
+
+
+
+
+
+
+
+
+
