2a3c15fdf0fd7347293bb91e5a8ca4193cad7bed
FidelMunywoki <fidelis.munywoki@student.moringaschool.com>
2021-07-15 20:54:19 +0300

    index.js

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
 
-function akanNames(){
-    var month = document.getElementById("month");
-    var day = document.getElementById("date");
-    var year = document.getElementById("year");
-    var gender = document.getElementsByName("gender");
-    var dob = month + " " + day + ", " + year;
+function akanNames(y, m, d, g){ 
+    var dob = m + " " + d + ", " + y;
     var then = new Date(dob);
     var theDay = then.getDay();
     //Days counting ===>>>>["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var akanMale = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
     var akanFemale = ['Akosua', 'Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
-    if (gender === 'Male'){
+    if (g === 'Male'){
         return akanMale[theDay];
     }
-    else if ( gender === 'Female'){
+    else if ( g === 'Female'){
         return akanFemale[theDay];
     }
 }
 
 
-function outputAkanNames(){
-    var 
-}
 
-
-
-akanName = akanNames();
-console.log(akanName);
-
-
-
-
-
+// User interface (or front-end) logic:
+$(document).ready(function() {
+    $("form#form").submit(function(event) {
+        event.preventDefault();
+        var y = parseInt($("#year").val());
+        var m = parseInt($("#month").val());
+        var d = parseInt($("#date").val());
+        var g = $("input:radio[name=gender]:checked").val();
+        var result = akanNames(y, m, d, g);
+        alert("Your akan name is: " + result);
+        //refresh page
+        document.getElementById("form").reset();
+    });
+});
 
 
 
